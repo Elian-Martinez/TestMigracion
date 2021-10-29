093ff259c02a62ba3740f36c1d2c6c05ecefd1db
Elian-Martinez <elianalbertomartinezcuevas@gmail.com>
2021-10-29 02:56:14 -0400

    Migrations_Complete

--- Migrations/20211029065305_Initial.Designer.cs
+++ Migrations/20211029065305_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Test_Migracion_.Models;
 
 namespace Test_Migracion_.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211029065305_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/AplicationDbContext.cs
+++ Models/AplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace Test_Migracion_.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Person DbSet
-            modelBuilder.Entity<Person>().Property(x => x.PersonId).HasColumnName("Id").UseIdentityColumn(1,1).IsRequired();
+            modelBuilder.Entity<Person>().Property(x => x.PersonId).HasColumnName("Id").IsRequired();
             modelBuilder.Entity<Person>().Property(x => x.PersonName).HasColumnName("Name").HasMaxLength(20).IsRequired();
             modelBuilder.Entity<Person>().Property(x => x.PersonLastName).HasColumnName("LastName").HasMaxLength(25).IsRequired();
             modelBuilder.Entity<Person>().Property(x => x.PersonOfBirth).HasColumnName("BirthdayDate").IsRequired();
@@ -25,15 +25,15 @@ namespace Test_Migracion_.Models
             modelBuilder.Entity<Person>().Property(x => x.PersonPhoto).HasColumnName("Photo").IsRequired();
 
             //Requests DbSet
-            modelBuilder.Entity<Requests>().Property(x => x.RequestsId).HasColumnName("Id").UseIdentityColumn(1, 1).IsRequired();
+            modelBuilder.Entity<Requests>().Property(x => x.RequestsId).HasColumnName("Id").IsRequired();
             modelBuilder.Entity<Requests>().Property(x => x.RequestsName).HasColumnName("Description").HasMaxLength(75).IsRequired();
             modelBuilder.Entity<Requests>().Property(x => x.RequestsEstate).HasColumnName("Estate").HasMaxLength(10).IsRequired();
             modelBuilder.Entity<Requests>().Property(x => x.RequestsCreation).HasColumnName("Creation").IsRequired();
 
             //Equipment DbSet
-            modelBuilder.Entity<Equipment>().Property(x => x.EquipmentId).HasColumnName("Id").UseIdentityColumn(1, 1).IsRequired();
-            modelBuilder.Entity<Equipment>().Property(x => x.PersonId).HasColumnName("PersonId").IsRequired();
-            modelBuilder.Entity<Equipment>().Property(x => x.RequestsId).HasColumnName("RequestsId").IsRequired();
+            modelBuilder.Entity<Equipment>().Property(x => x.EquipmentId).HasColumnName("Id").IsRequired();
+            modelBuilder.Entity<Equipment>().Property(x => x.PersonId).IsRequired();
+            modelBuilder.Entity<Equipment>().Property(x => x.RequestsId).IsRequired();
             modelBuilder.Entity<Equipment>().Property(x => x.CreationDate).HasColumnName("CreationDate").IsRequired();
         }
         public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
